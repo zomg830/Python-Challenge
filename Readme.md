6a62a224e6c50b24d789e6a73130020e6286d9c2
zomg830 <43345428+zomg830@users.noreply.github.com>
2019-01-30 09:14:51 -0600

    Add files via upload

---
+++ Readme.md
@@ -0,0 +1,94 @@
+# Elevator Problem
+
+Priorities:
+ 1. Correctness
+ 2. Implementation time
+ 3. Elegance of code
+ 4. Anything else you think we might care about in a coding test. 
+
+The following diagram (given as a multi-line string) is an elevator state – the state of an elevator system at a given point in time.  Dots represent an elevator shaft.  Letters (specifically A-Z) represent an elevator – for example, elevator A is on the 1st floor (1-indexed), and the following elevator state has 6 floors:
+
+ xx.x.x.xDxx
+ xx.x.x.x.xx
+ xx.x.x.x.xx
+ xx.xBx.x.xx
+ xx.x.xCx.xx
+ xxAx.x.x.xx
+
+
+Goal: Take as input a series of elevator states from an array, representing successive states of the elevator system from time t = {1,2,3…}.  Valid elevators are specified by the characters A-Z.
+
+Write a function that takes an array of elevator states, the starting elevator, and the final destination (floor) desired by the user.  Your function should be similar to:
+
+findElevatorPath(elevatorStates, startingElevator, finalDestination)
+
+
+Where <finalDestination> is a string specified as <floor>-<time>, e.g. “3-2”, indicating that the final destination is the 3rd floor at time t=2.
+
+
+At t=1, you begin in the elevator specified by <startingElevator>, e.g. “A”.
+
+In a given time period, you can board an elevator or stay on the elevator you are on. In any time period, you can only board any elevator on the same floor as the elevator that you are currently on (including t=1).
+
+
+The goal is to find a series of legal actions that lead to you being at the final destination – the right floor at the right time.  The set of actions should be returned from the function as a series of actions in a single string. An action is defined as the elevator you board (or stay on) at time t, eg:
+
+
+“AABDD”
+
+
+Indicates that you were on (or switched to) A at t=1, stayed on A at t=2, switched to B at t=3, and switched to D at t=4, and stayed on D at t=5.
+
+If there is a solution, the solution string is the *only* thing that should be returned by the function.  If there is no solution, return something appropriate (e.g., “NO SUCCESSFUL ROUTE”).
+
+
+Sample Input:
+
+
+elevatorStates = [
+ // State @ t=1
+ `xx.x.x.xDxx
+  xx.x.x.x.xx
+  xx.x.x.x.xx
+  xx.xBx.x.xx
+  xx.x.xCx.xx
+  xxAx.x.x.xx`,
+ // State @ t=2
+ `xx.x.x.x.xx
+  xx.x.x.x.xx
+  xxAx.x.x.xx
+  xx.xBx.x.xx
+  xx.x.xCx.xx
+  xx.x.x.xDxx`,
+ // State @ t=3
+ `xx.x.xCx.xx
+  xx.x.x.x.xx
+  xx.x.x.x.xx
+  xxAxBx.x.xx
+  xx.x.x.x.xx
+  xx.x.x.xDxx`,
+ // State @ t=4
+ `xx.x.xCx.xx
+  xx.x.x.x.xx
+  xx.xBx.xDxx
+  xx.x.x.x.xx
+  xxAx.x.x.xx
+  xx.x.x.x.xx`,
+  // State @ t=5
+ `xx.x.xCx.xx
+  xx.x.x.xDxx
+  xx.x.x.x.xx
+  xx.x.x.x.xx
+  xxAxBx.x.xx
+  xx.x.x.x.xx`,
+]
+
+
+startingElevator = "A"
+finalDestination = "5-5"
+
+
+
+
+Sample Output:
+"AABDD"
---
+++ elevator-challenge.py
@@ -0,0 +1,168 @@
+# Importing re to use later for searching via regular expressions
+# anytree will be used to create a tree data structure 
+import re
+import anytree
+from anytree import Node, RenderTree
+
+# Elevator states inputs, had to change initial formatting to import multi-line data properly using the ''' data, data ''' notation 
+states = [
+ # State @ t=1
+ '''xx.x.x.xDxx
+ xx.x.x.x.xx
+ xx.x.x.x.xx
+ xx.xBx.x.xx
+ xx.x.xCx.xx
+ xxAx.x.x.xx''',
+ # State @ t=2
+ '''xx.x.x.x.xx
+ xx.x.x.x.xx
+ xxAx.x.x.xx
+ xx.xBx.x.xx
+ xx.x.xCx.xx
+ xx.x.x.xDxx''',
+ # State @ t=3
+ '''xx.x.xCx.xx
+ xx.x.x.x.xx
+ xx.x.x.x.xx
+ xxAxBx.x.xx
+ xx.x.x.x.xx
+ xx.x.x.xDxx''',
+ # State @ t=4
+ '''xx.x.xCx.xx
+ xx.x.x.x.xx
+ xx.xBx.xDxx
+ xx.x.x.x.xx
+ xxAx.x.x.xx
+ xx.x.x.x.xx''',
+ # State @ t=5
+ '''xx.x.xCx.xx
+ xx.x.x.xDxx
+ xx.x.x.x.xx
+ xx.x.x.x.xx
+ xxAxBx.x.xx
+ xx.x.x.x.xx''',
+# State @ t=6
+ '''xx.x.x.x.xx
+ xxAx.x.x.xx
+ xx.xBx.x.xx
+ xx.x.x.x.xx
+ xx.x.x.x.xx
+ xx.x.xCxDxx''',
+ # State @ t=7
+ '''xx.x.x.x.xx
+ xx.x.xCx.xx
+ xxAx.x.x.xx
+ xx.x.x.xDxx
+ xx.xBx.x.xx
+ xx.x.x.x.xx'''
+]
+
+# Primary function call
+def findElevatorPath(elevatorStates,startingElevator,finalDestination):
+    # Trimming the data to get a simplified list format
+    b = "x.\n"
+    i = 0
+    if (type(elevatorStates[i]) is not list):
+        while i < len(elevatorStates):
+            for char in b:
+                elevatorStates[i] = elevatorStates[i].replace(char,"")
+            elevatorStates[i] = elevatorStates[i].split(" ")
+            i+=1
+
+    # Displays the trimmed data
+    # print(elevatorStates)
+
+    # Calculates the top floor of the "building"
+    def top(stateTime):
+        topFloor = 0
+        for str in elevatorStates[0]:
+            topFloor+=1
+        return topFloor
+
+    # Takes in a state at t=n and an elevator string to find
+    # the floor index of the elevator
+    def floor(stateTime,elevatorStr):
+        i = 0
+        for str in stateTime:
+            if (str.find(elevatorStr) > -1):
+                return i
+            i+=1
+
+    # Instantiating initial variables for the function
+    starting = startingElevator
+    final = finalDestination
+    finalTimeInd = int(final[2])-1
+    finalFloorInd = top(elevatorStates[0]) - int(final[0])
+    finalPos = (finalTimeInd,finalFloorInd)
+
+    # Takes in a state at t=n and a floor index to find the
+    # elevators currently on the floor
+    def elevators(stateTime, floorIndex):
+        elevators = []
+        for str in stateTime[floorIndex]:
+            match = re.findall(r'[A-Z]',str)
+            elevators += match
+        return elevators
+
+    # Creating the initial root directory and populating it with the t = 0 values, global variable instantiation not ideal
+    root = Node("root")
+    def newNodes(stateTime):
+        time = 0
+        for str in stateTime:
+            if (str != ""):
+                globals()[str+time.__str__()] = Node(stateTime[floor(stateTime,str)],parent=root,pos = (time,floor(stateTime,str)))
+    newNodes(elevatorStates[0])
+
+    # Populates the nodes based on the states input, once again global variables not ideal (hacker-fix)
+    def addNodes(state,time,x):
+        for str in state[time]:
+            # If statement for floor with only one elevator
+            if (str != "" and len(str) == 1):
+                    globals()[str+time.__str__()] =  Node(state[time][floor(state[time],str)],parent=globals()[str+(time-1).__str__()],pos = (time,floor(state[time],str)))
+            # If statement for floor with 2+ elevators
+            elif (str != "" and len(str) > 1):
+                    elevator = elevators(elevatorStates[time],floor(elevatorStates[time],str))
+                    for i in elevator:
+                            globals()[i+time.__str__()] =  Node(i,parent=globals()[str[x]+(time-1).__str__()],pos = (time,floor(state[time],i)))
+
+    def printResult():
+        # Populates the nodes into the tree data structure
+        time = 1
+        while (time < len(elevatorStates)):
+            # If statements are due to overwrite of node variables (e.g. A2) depending on the string
+            # index of the elevators on floors with 2+ elevators, probably will not scale well with additional elevator shafts
+            if (starting == "B" or starting == "D"):
+                addNodes(elevatorStates,time,0)
+                addNodes(elevatorStates,time,1)
+            elif (starting == "A" or starting == "C"):
+                addNodes(elevatorStates,time,1)
+                addNodes(elevatorStates,time,0)
+            time+=1
+
+        # Renders the tree for easier readability
+        # print(RenderTree(root))
+        result = anytree.search.findall_by_attr(root, finalPos, "pos")
+        # print("result = ", result)
+        if (result == ()):
+            print("error: no path found")
+        else:
+            for i in range(len(result)):
+                match = re.findall(r'/[A-Z]',result[i].__str__())
+                # print("match = ", match)
+            x=""
+            for str in match:
+                x += str[1]
+            if (starting == x[0]):
+                print(x)
+            else:
+                print("error: path not found")
+
+    printResult()
+
+
+findElevatorPath(states,"A","5-5")
+findElevatorPath(states,"C","6-5")
+findElevatorPath(states,"B","4-4")
+findElevatorPath(states,"D","1-2")
+findElevatorPath(states,"D","5-6") # Added t = 6 case to test
+findElevatorPath(states,"C","3-7") # Added t = 7 case to test
